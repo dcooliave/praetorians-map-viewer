db40e323aca2f336c9962a9681464e585a646e9e
badbrainz <wormboy.d@gmail.com>
2021-06-26 09:37:49 -0700

    Calculate tile flag in vertex shader

--- src/load-tile-material.js
+++ src/load-tile-material.js
@@ -13,7 +13,6 @@ export default function createMaterial() {
       varying vec3 vTopUV;
       varying vec3 vBottomUV;
       varying vec4 vVertexColor;
-      varying vec3 vTileFlag;
 
       void main() {
         vec2 layer = aCoordinate.xy;
@@ -33,8 +32,11 @@ export default function createMaterial() {
         vTopUV = vec3(uvtile, layer.x);
         vBottomUV = vec3(uvtile, layer.y);
 
-        vVertexColor = data0;
-        vTileFlag = aFlag;
+        vec4 color = data0;
+        color = mix(color, vec4(0.0, 1.0, 0.0, 0.0), aFlag.x * 0.6);
+        color = mix(color, vec4(1.0, 0.0, 0.0, 0.0), aFlag.y * 0.6);
+        color = mix(color, vec4(0.0, 0.0, 1.0, 0.0), aFlag.z * 0.6);
+        vVertexColor = color;
 
         vec4 vert = vec4(position, 1.0);
         vert.y += data1.r * (uElevation.x - uElevation.y) + uElevation.y;
@@ -48,20 +50,11 @@ export default function createMaterial() {
       varying vec3 vTopUV;
       varying vec3 vBottomUV;
       varying vec4 vVertexColor;
-      varying vec3 vTileFlag;
 
       void main() {
         vec4 top = texture(uTexture, vTopUV);
         vec4 bottom = texture(uTexture, vBottomUV);
-
-        vec4 logic = vec4(1.0, 0.0, 0.0, 0.0);
-        vec4 type = vec4(0.0, 1.0, 0.0, 0.0);
-        vec4 tile = vec4(0.0, 0.0, 1.0, 0.0);
-
         vec4 color = mix(bottom, top, vVertexColor.a) * vVertexColor;
-        color = mix(color, type.bgra, vTileFlag.x * 0.6);
-        color = mix(color, logic.bgra, vTileFlag.y * 0.6);
-        color = mix(color, tile.bgra, vTileFlag.z * 0.6);
 
         gl_FragColor = vec4(color.bgr, 1.0);
       }
