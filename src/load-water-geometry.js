f4444e5a1b6ec67a3886fab447d58411b351b251
badbrainz <wormboy.d@gmail.com>
2021-07-21 22:41:49 -0700

    Use nature loaders for water

--- src/load-water-geometry.js
+++ src/load-water-geometry.js
@@ -1,37 +1,3 @@
-import {
-  BufferGeometry,
-  InterleavedBuffer,
-  InterleavedBufferAttribute,
-  Uint16BufferAttribute
-} from './three/build/three.module.js'
+import loadNatureGeometry from './load-nature-geometry.js'
 
-export default function(vertices, indices) {
-  const size = 9
-  const data = []
-
-  for (let i = 0; i < vertices.numVertices; i++) {
-    data.push(
-      vertices.points[i * 3],
-      vertices.points[i * 3 + 1],
-      vertices.points[i * 3 + 2],
-
-      vertices.colors[i * 4] / 255,
-      vertices.colors[i * 4 + 1] / 255,
-      vertices.colors[i * 4 + 2] / 255,
-      vertices.colors[i * 4 + 3] / 255,
-
-      vertices.uv[i * 2],
-      vertices.uv[i * 2 + 1]
-    )
-  }
-
-  const buffer = new InterleavedBuffer(new Float32Array(data), size)
-
-  const geometry = new BufferGeometry()
-  geometry.setAttribute('position', new InterleavedBufferAttribute(buffer, 3, 0))
-  geometry.setAttribute('color', new InterleavedBufferAttribute(buffer, 4, 3))
-  geometry.setAttribute('uv', new InterleavedBufferAttribute(buffer, 2, 7))
-  geometry.setIndex(new Uint16BufferAttribute(indices, 1))
-
-  return geometry
-}
+export default loadNatureGeometry
--- src/load-water-texture.js
+++ src/load-water-texture.js
@@ -1,20 +1,10 @@
-import {
-  DataTexture,
-  LinearFilter,
-  LinearMipMapLinearFilter,
-  RepeatWrapping,
-  RGBAFormat
-} from './three/build/three.module.js'
+import loadNatureTexture from './load-nature-texture.js'
 
 export default function(image) {
-  const texture = new DataTexture(image, 256, 256)
-
-  texture.magFilter = LinearFilter
-  texture.minFilter = LinearMipMapLinearFilter
-  texture.wrapS = RepeatWrapping
-  texture.wrapT = RepeatWrapping
-  texture.format = RGBAFormat
-  texture.generateMipmaps = true
-
-  return texture
+  return loadNatureTexture({
+    image,
+    width: 256,
+    height: 256,
+    bitsPerPixel: 32
+  })
 }
