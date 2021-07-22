dda67c9bddb609708453a4bc9a25842912e90508
badbrainz <wormboy.d@gmail.com>
2021-07-21 22:15:10 -0700

    Use interleaved buffer for nature

--- src/load-nature-geometry.js
+++ src/load-nature-geometry.js
@@ -1,17 +1,36 @@
 import {
   BufferGeometry,
-  Float32BufferAttribute,
+  InterleavedBuffer,
+  InterleavedBufferAttribute,
   Uint16BufferAttribute
 } from './three/build/three.module.js'
 
 export default function(vertices, indices) {
+  const size = 9
+  const data = []
+
+  for (let i = 0; i < vertices.numVertices; i++) {
+    data.push(
+      vertices.points[i * 3],
+      vertices.points[i * 3 + 1],
+      vertices.points[i * 3 + 2],
+
+      vertices.colors[i * 4] / 255,
+      vertices.colors[i * 4 + 1] / 255,
+      vertices.colors[i * 4 + 2] / 255,
+      vertices.colors[i * 4 + 3] / 255,
+
+      vertices.uv[i * 2],
+      vertices.uv[i * 2 + 1]
+    )
+  }
+
+  const buffer = new InterleavedBuffer(new Float32Array(data), size)
+
   const geometry = new BufferGeometry()
-
-  const colors = vertices.colors.map(c => c / 255)
-
-  geometry.setAttribute('position', new Float32BufferAttribute(vertices.points, 3))
-  geometry.setAttribute('color', new Float32BufferAttribute(colors, 4))
-  geometry.setAttribute('uv', new Float32BufferAttribute(vertices.uv, 2))
+  geometry.setAttribute('position', new InterleavedBufferAttribute(buffer, 3, 0))
+  geometry.setAttribute('color', new InterleavedBufferAttribute(buffer, 4, 3))
+  geometry.setAttribute('uv', new InterleavedBufferAttribute(buffer, 2, 7))
   geometry.setIndex(new Uint16BufferAttribute(indices, 1))
 
   return geometry
