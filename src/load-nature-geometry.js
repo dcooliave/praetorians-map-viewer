9feb42ba2b049e7922cd7b7972ed6e608f9bed8f
badbrainz <wormboy.d@gmail.com>
2021-07-24 23:57:06 -0700

    Store interleaved color as 4 bytes

--- src/load-nature-geometry.js
+++ src/load-nature-geometry.js
@@ -6,31 +6,38 @@ import {
 } from './three/build/three.module.js'
 
 export default function(vertices, indices) {
-  const size = 9
-  const data = []
+  const size = 6
+  const buffer = new ArrayBuffer(vertices.numVertices * size * 4)
+  const floats = new Float32Array(buffer)
+  const bytes = new Uint8Array(buffer)
 
   for (let i = 0; i < vertices.numVertices; i++) {
-    data.push(
-      vertices.points[i * 3],
-      vertices.points[i * 3 + 1],
-      vertices.points[i * 3 + 2],
+    let offset = i * size
 
-      vertices.colors[i * 4] / 255,
-      vertices.colors[i * 4 + 1] / 255,
-      vertices.colors[i * 4 + 2] / 255,
-      vertices.colors[i * 4 + 3] / 255,
+    floats[offset++] = vertices.points[i * 3]
+    floats[offset++] = vertices.points[i * 3 + 1]
+    floats[offset++] = vertices.points[i * 3 + 2]
 
-      vertices.uv[i * 2],
-      vertices.uv[i * 2 + 1]
-    )
+    offset = i * size * 4 + 12
+
+    bytes[offset++] = vertices.colors[i * 4]
+    bytes[offset++] = vertices.colors[i * 4 + 1]
+    bytes[offset++] = vertices.colors[i * 4 + 2]
+    bytes[offset++] = vertices.colors[i * 4 + 3]
+
+    offset = i * size + 4
+
+    floats[offset++] = vertices.uv[i * 2]
+    floats[offset++] = vertices.uv[i * 2 + 1]
   }
 
-  const buffer = new InterleavedBuffer(new Float32Array(data), size)
+  const floatBuffer = new InterleavedBuffer(floats, size)
+  const byteBuffer = new InterleavedBuffer(bytes, size * 4)
 
   const geometry = new BufferGeometry()
-  geometry.setAttribute('position', new InterleavedBufferAttribute(buffer, 3, 0))
-  geometry.setAttribute('color', new InterleavedBufferAttribute(buffer, 4, 3))
-  geometry.setAttribute('uv', new InterleavedBufferAttribute(buffer, 2, 7))
+  geometry.setAttribute('position', new InterleavedBufferAttribute(floatBuffer, 3, 0))
+  geometry.setAttribute('color', new InterleavedBufferAttribute(byteBuffer, 4, 12, true))
+  geometry.setAttribute('uv', new InterleavedBufferAttribute(floatBuffer, 2, 4))
   geometry.setIndex(new Uint16BufferAttribute(indices, 1))
 
   return geometry
